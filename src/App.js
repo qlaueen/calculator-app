c79a44dc997710fb18e2f1899c4a4400e2d0cfcd
qlaueen <qlaueen@gmail.com>
2023-01-05 22:54:38 -0500

    creating calculator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import React, { useReducer } from 'react';
+
+function reducer (state, action) {
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [state, dispatch] = useReducer(reducer, {});
+
+  return(
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <button className="span-two">AC</button>
+      <button className='special-btn'>DEL</button>
+      <button className='special-btn'>÷</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button className='special-btn'>x</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button className='special-btn'>+</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button className='special-btn'>-</button>
+      <button className='special-btn'>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
-  );
+  )
 }
 
 export default App;
