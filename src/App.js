c35b2ff913a0fa4e2a2813cfe9a3b49cc6f81df2
qlaueen <qlaueen@gmail.com>
2023-01-06 17:17:29 -0500

    bug fixes

--- src/App.js
+++ src/App.js
@@ -116,10 +116,10 @@ const INTEGER_FORMATTER = new Intl.NumberFormat('en-US', {
 });
 
 function formatOperand(operand) {
-  if (operand == null) return '';
-  const[integer, decimal] = operand.split('.');
-  if (decimal == null) return INTEGER_FORMATTER.format(integer);
-  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`; 
+  if (operand == null) return
+  const [integer, decimal] = operand.split(".")
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
 }
 
 function App() {
